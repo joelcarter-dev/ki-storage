0928065b9c7daf217962a0782436946aea6e277d
Dwaxy <joel.innes@gmail.com>
2018-08-13 11:12:22 +1200

    init commint

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import './main.sass'
+
+export default () => (
+  <section className="main-holder">
+    <div className="title">
+      <h1>Mangakino Storage</h1>
+      <p id="phone"> Phone 021864254 </p>
+      <h2>Email MangoBoatStorage@gmail.com</h2>
+      <p>T & C apply</p>
+    </div>
+  </section>  
+)
